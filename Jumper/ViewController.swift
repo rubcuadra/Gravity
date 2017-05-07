6a88bdf01e94abbbfe5ecd42d50dd023a8c6e476
rubcuadra <rubcuadra@gmail.com>
2017-05-06 22:13:20 -0500

    :tada:

---
+++ Jumper/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Jumper
+//
+//  Created by Ruben Cuadra on 5/6/17.
+//  Copyright © 2017 Ruben Cuadra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        self.view.wantsLayer = true
+        self.view.layer?.backgroundColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1.0)
+        let font: NSFont = .systemFont(ofSize: 30)
+        // Do any additional setup after loading the view.
+    }
+    
+    public func updateLabel(Score: Int)
+    {
+        //label.stringValue = String(describing: Score)
+    }
+    
+    override var representedObject: Any?
+        {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
